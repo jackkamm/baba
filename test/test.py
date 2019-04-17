d202da4826655ee642fe345aca8cea9bf657d5db
Jack Kamm <jackkamm@gmail.com>
2019-04-17 07:19:44 -0700

    Remove test that relies on missing file

--- test/test.py
+++ test/test.py
@@ -5,12 +5,12 @@ import os.path as osp
 import autograd.numpy as np
 import pandas as pd
 
-def test_quartet_decomp_df():
-    path = osp.join(osp.dirname(osp.abspath(__file__)), "inferred_components.txt")
-    qd = baba.quartet_decomposition.from_dataframe(path)
-    qd_df = qd.data_frame()
-    qd_df2 = pd.read_table(path)
-    assert qd_df.equals(qd_df2)
+#def test_quartet_decomp_df():
+#    path = osp.join(osp.dirname(osp.abspath(__file__)), "inferred_components.txt")
+#    qd = baba.quartet_decomposition.from_dataframe(path)
+#    qd_df = qd.data_frame()
+#    qd_df2 = pd.read_table(path)
+#    assert qd_df.equals(qd_df2)
 
 
 def test_get_permutations():

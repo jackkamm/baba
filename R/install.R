fe40e7980a5215673d2df0c9f3582e9f53533e4e
Jack Kamm <jackkamm@gmail.com>
2019-04-16 18:25:16 -0700

    Delete old R package

--- R/install.R
+++
@@ -1,9 +0,0 @@
-# meant to be run from file directory
-library(devtools)
-if (require("baba")){
-	remove.packages("baba")
-}
-setwd("baba")
-build()
-document()
-install()
--- R/scripts/run_baba.R
+++
@@ -1,4 +0,0 @@
-library(baba)
-
-args <- commandArgs(trailingOnly=TRUE)
-do.call(args[1], as.list(args[-1]))
